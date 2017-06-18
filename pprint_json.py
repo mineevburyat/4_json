2a88d3b1e077a6218a094faacfdc99088842a5b7
Mineev <mineev.buryat@gmail.com>
2017-06-18 19:38:40 +0800

    реализованы функции загрузки json и вывода внутренних данных на экран

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,29 @@
 import json
-
+import sys
 
 def load_data(filepath):
-    pass
-
+    try:
+        fh = open(filepath,'r')
+    except FileNotFoundError:
+        print("Файл не найден!")
+        exit(1)
+    except OSError:
+        print("Неожиданная ошибка!")
+        exit(1)
+    try:
+        data = json.load(fh)
+    except:
+        print("Это не json формат!")
+        exit(1)
+    return data
 
 def pretty_print_json(data):
-    pass
+    """Выводить каждый параметром с отступом в два пробела и сортировать, учитывать UTF-8"""
+    print(json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        print("Ожидается путь к файлу json.")
+    else:
+        pretty_print_json(load_data(sys.argv[1]))
